b69630e006a6f510d5e8ecbe9c5737659cda2a0c
tetsu5555 <tetete1118@gmail.com>
2021-06-10 08:19:51 +0900

    fix

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"load-balancer/pool"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 	InitRoutes(router)
+	pool.InitServerPool()
 	router.Run(":8080")
 }
--- pool/serverpool.go
+++ pool/serverpool.go
@@ -6,8 +6,8 @@ var serverPool *models.ServerPool
 
 func InitServerPool() {
 	serverPool = models.NewServerPool()
-	server1 := models.GetNewServer("http://localhost:9000")
-	server2 := models.GetNewServer("http://localhost:9000")
+	server1 := models.GetNewServer("http://localhost:7000")
+	server2 := models.GetNewServer("http://localhost:7000")
 	serverPool.RegisterServer(server1)
 	serverPool.RegisterServer(server2)
 }
